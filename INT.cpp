60005ec7ef8c636cf451a8c40f3a9077594e270c
cmurph14 <cmurph14@villanova.edu>
2010-10-11 15:01:26 -0400

    Getting closer!

--- INT.cpp
+++ INT.cpp
@@ -1,7 +1,89 @@
 #include "INT.h"
 
-INT operator=(INT &y)
+INT::INT(int y)
+{
+    x = y;
+}
+INT::INT()
+{
+    x;
+}
+
+INT& INT::operator=(INT &y)
 {
     x = y.get();
-    return x;
+    return *this;
+}
+
+INT& INT::operator=(int y)
+{
+    x = y;
+    return *this;
+}
+INT INT::operator+(INT &y)
+{
+    return INT(x + y.get());
+}
+
+INT INT::operator-(INT &y)
+{
+    return INT(x - y.get());
+}
+
+INT INT::operator/(INT &y)
+{
+    return INT(x / y.get());
+}
+
+INT INT::operator*(INT &y)
+{
+    return INT(x * y.get());
+}
+
+INT& INT::operator++()
+{
+    x++;
+    return *this;
+}
+
+INT INT::operator++(int)
+{
+    x++;
+    return *this;
+}
+
+INT& INT::operator--()
+{
+    x--;
+    return *this;
+}
+
+INT INT::operator--(int)
+{
+    x--;
+    return *this;
+}
+
+INT& INT::operator+=(INT &y)
+{
+    x += y.get();
+    return *this;
+}
+
+INT& INT::operator+=(int y)
+{
+    x += y;
+    return *this;
+}
+
+INT& INT::operator-=(INT &y)
+{
+    x -= y.get();
+    return *this;
+}
+
+INT& INT::operator-=(int y)
+{
+    x -= y;
+    return *this;
 }
